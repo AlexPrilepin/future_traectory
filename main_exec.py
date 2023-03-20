6b197dffeddbce21020e8c9e05155f39edbc213c
AlexPrilepin <111271681+AlexPrilepin@users.noreply.github.com>
2023-03-20 10:03:59 +0200

    Add files via upload

--- main_exec.py
+++ main_exec.py
@@ -1,15 +1,64 @@
 from csv_analizer import csv_to_list
 from request_maker import One_request, Single_port_request
+import urllib
+from urllib import request
 
 
-list_servers = csv_to_list()
+def execution():
+	try:
+		list_servers = csv_to_list()
+	except Exception:
+		print("IT SEEMS THAT EITHER THE CSV TABLE DOES NOT EXIST OR ITS NAME OR/AND DIRECTORY HAS BEEN CHANGED")
+		return
+	domains = []
+	final_results = []
 
 
-for i in list_servers:
-    try:
-        on = One_request(i[0], i[1])
-    except Exception:
-        if i[0].split('.') in [1, 3]:
-            on = One_request(i[0], None)
-        else:
-            on = One_request(None, i[1])
+	try:
+	    urllib.request.urlopen("http://google.com")
+	except IOError:
+	    "OOOPS!!! Internet seems to be absent on your device!"
+	    return
+
+
+	for i in list_servers:
+		if len(i) != 2:
+			print('WRONG INPUT GIVEN')
+			return
+		elif i[0] == '':
+			print('NULL SPACE IN STEAD OF IP_ADRESS/DOMAIN GIVEN')
+			return
+
+	for i in list_servers:
+	    try:
+	        on = One_request(i[0], i[1])
+	    except Exception:
+	        if i[0].split('.') in [1, 3]:
+	            on = One_request(i[0], None)
+	        else:
+	            on = One_request(None, i[1])
+	    domains.append(on)
+	for i in domains:
+		main_info = [i.host if i.host.count('.') < 2 else '???', i.ip_adresses, i.ports]
+		big_lines = []
+		for j in i.requests:
+			stdout = []
+			stdout.append(str(j.time_of_request))
+			stdout.append(i.host if i.host.count('.') < 2 else '???')
+			stdout.append(str(j.ip_adress))
+			stdout.append(str(j.packets_loss))
+			stdout.append(str(j.rtt_max_ms)) 
+			stdout.append(str(j.port) if str(j.port).isdigit() else '-1')
+			stdout.append(str(j.state) )
+			big_lines.append('\t' + ' ||| '.join(stdout))
+		final_results.append([main_info, big_lines, i.message])
+
+
+	for i in final_results:
+		print(i[0])
+		for j in i[1]:
+			print(j)
+		print(i[2])
+		print('\n' + '-' * 20 + '\n')
+
+	print()
--- request_maker.py
+++ request_maker.py
@@ -1,21 +1,6 @@
 import socket
 from pythonping import ping
 import datetime
-import whois
-
-##WTF PING DOES NOT ; ADMIN NEED PERMISSION
-##print(ping('8.8.8.8').__dict__)
-##print(ping('8.8.8.8', count=1).__dict__['_responses'])
-
-##domain = whois.query('google.com')
-
-
-##AS WELL AS WHOIS
-##print(domain.__dict__)
-##import os
-##hostname = "google.com" #example
-##response = os.system("ping -c 1 " + hostname)
-##
 
 
 class One_request:
@@ -23,51 +8,83 @@ class One_request:
         self.host = host
         self.ports = ports
         self.requests = []
+        self.message = ''
+        try:
+            self.ip_adresses = socket.gethostbyname_ex(self.host)[2] if host != None else []
+        except Exception:
+            self.ip_adresses = []
+            self.message = '\t\nDUE TO THE SYSTEM ANSWER, POSSIBLY THIS WEBSITE DOES NOT EXIST'
         self.group_requester()
+        for i in self.ports:
+            if i.isdigit() is False:
+                del self.ports[self.ports.index(i)]
 
-    def make_single_request(self, port):
-        obj = Single_port_request(self.host, port)
+
+    def make_single_request(self, port, ip=None):
+        obj = Single_port_request(self.host, port) if self.ip_adresses == [] else  Single_port_request(self.host, port, ip)
         self.requests.append(obj)
 
     def group_requester(self):
-        for i in self.ports:
-            self.make_single_request(i)
+        if not self.ip_adresses:
+            for i in self.ports:
+                self.make_single_request(i)
+        else:
+            for i in self.ports:
+                for j in self.ip_adresses:
+                    self.make_single_request(i, j)
     
 class Single_port_request:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
+    def __init__(self, host, port, ip=None):
+        self.host = ip if ip!= None else host 
+        self.port = port if port.isdigit() else '???'
         self.time_of_request = None
         self.ip_adress = None
         self.rtt_max_ms = '2000 ms'
         self.state = False
-        self.packets = 1.0
+        self.packets_loss = 0.0
         self.make_request()
+        if self.ip_adress == None:
+            if self.host.count('.') == 3:
+                self.ip_adress = self.host
+        if self.ip_adress == None:
+            self.ip_adress = '???'
         
     def make_request(self):
         self.time_of_request = datetime.datetime.now()
+
         try:
-            pg = str(ping('8.8.8.8', count=1).__dict__['_responses'][0]).split()[-1]
-            print(pg)
-            self.packets = 1.0
-            self.rtt_max_ms = pg
-            conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            conn.connect((self.host, int(self.port)))
-            conn_for_ip = str(conn).split('=')
-            flag1 = False
-            for i in conn_for_ip:
-                if flag1:
-                    self.ip_adress = i.split(', ')[0].strip('<>').strip('()').strip("'")
-                    break
-                if 'raddr' in i:
-                    flag1 = True
+            zzz = ping(self.host, count=1, timeout=2)
+            if str(zzz).split()[0].lower() == 'reply':
+                pg = str(zzz.__dict__['_responses'][0]).split()[-1]
+                self.rtt_max_ms = pg 
+            else:
+                self.packets_loss = 1.0
+        except Exception:
+            pass
 
-            print()
-            self.state = True
-############ MAKE WHOIS AND PYTHONPING REQUESTS
-        except Exception as ex:
-            print(type(ex), ex)
-            print()
+        if self.port != '???':
+            try:
+                conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                conn.settimeout(8)
+                conn.connect((self.host, int(self.port)))
+                conn_for_ip = str(conn).split('=')
+                flag1 = False
+                for i in conn_for_ip:
+                    if flag1:
+                        self.ip_adress = i.split(', ')[0].strip('<>').strip('()').strip("'")
+                        break
+                    if 'raddr' in i:
+                        flag1 = True
 
+                self.state = True
+            except Exception as ex:
+                if str(type(ex)) == "<class 'socket.timeout'>":
+                    self.packets_loss = 1.0
+        else:
+            self.port = -1
+            self.state = '???'
 
-
+        if self.state == False:
+            self.state = 'Not Opened'
+        elif self.state == True:
+            self.state = 'Opened'
